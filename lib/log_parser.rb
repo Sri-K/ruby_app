e4c92a24ce32ee7bd34577e98e8948f8f156daf5
Sri Kalai <sriganeshkalai@gmail.com>
2019-10-03 12:13:31 +0100

    add parser script to execute in a command line and added more validations

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -2,6 +2,15 @@ class LogParser
   def initialize(logfile)
     @logfile = logfile
 
-    raise "File not found: #{logfile}" unless File.exist?(logfile)
+    raise "File not found" if logfile.length.zero?
+    raise "File or the directory not exists: #{logfile}" unless File.exist?(logfile)
   end
+
+  def parse
+  end
+end
+
+if ARGV[0]
+  logfile_parser = LogParser.new(ARGV[0])
+  logfile_parser.parse
 end
--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -5,10 +5,18 @@ RSpec.describe LogParser do
 
   describe '#initialize' do
     context 'when initialize without a file' do
-      let(:logfile) { "spec/fixtures/files/fakeserver.log" }
+      let(:logfile) { "" }
 
       it 'raises an error' do
-        expect { subject }.to raise_error(RuntimeError, "File not found: #{logfile}")
+        expect { subject }.to raise_error(RuntimeError, "File not found")
+      end
+    end
+
+    context 'when initialize with a fake file' do
+      let(:logfile) { "spec/fixtures/files/fakefile.log" }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error(RuntimeError, "File or the directory not exists: #{logfile}")
       end
     end
 
@@ -22,3 +30,4 @@ RSpec.describe LogParser do
   end
   
 end
+
